9283952c7644e758870657fe0b569701f65cd2c3
arrput <ari21putra@gmail.com>
2023-10-25 17:08:08 +0700

    LoginActivity

--- app/src/main/java/com/biie/tenantfeedback/activity/LoginActivity.java
+++ app/src/main/java/com/biie/tenantfeedback/activity/LoginActivity.java
@@ -2,15 +2,27 @@ package com.biie.tenantfeedback.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.biie.tenantfeedback.MainActivity;
 import com.biie.tenantfeedback.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btn_login);
+
+        login();
+    }
+
+    void login(){
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
     }
 }
