0ee9ffa3bb07c4981624a00154caa7b7305401ae
Daffa <daffasyah7890@gmail.com>
2023-10-31 11:52:58 +0700

    update profile

--- app/src/main/java/com/biie/tenantfeedback/activity/LoginActivity.java
+++ app/src/main/java/com/biie/tenantfeedback/activity/LoginActivity.java
@@ -1,28 +1,55 @@
 package com.biie.tenantfeedback.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 import com.biie.tenantfeedback.MainActivity;
 import com.biie.tenantfeedback.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button btnLogin;
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private Button mLoginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btnLogin = findViewById(R.id.btn_login);
-
-        login();
+        // Initialize views
+        mEmailEditText = findViewById(R.id.email);
+        mPasswordEditText = findViewById(R.id.password);
+        mLoginButton = findViewById(R.id.login_button);
+        final EditText passwordInput = findViewById(R.id.password);
+        // Set click listener for login button
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the entered email and password
+                String email = mEmailEditText.getText().toString();
+                String password = mPasswordEditText.getText().toString();
+            }
+        });
     }
-
-    void login(){
-        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-    }
+//    Button btnLogin;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+//
+//        btnLogin = findViewById(R.id.btn_login);
+//
+//        login();
+//    }
+//
+//    void login(){
+//        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//    }
 }
--- app/src/main/java/com/biie/tenantfeedback/activity/MainReport.java
+++ app/src/main/java/com/biie/tenantfeedback/activity/MainReport.java
@@ -1,78 +1,133 @@
 package com.biie.tenantfeedback.activity;
 
-public class MainReport {
-
-import android.support.v7.app.AppCompatActivity;
-import android.widget.AdapterView;
-import android.view.View;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.biie.tenantfeedback.R;
+
+public class MainReport {
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-    // Main Activity implements Adapter view
-    public class MainActivity
-            extends AppCompatActivity
-            implements AdapterView.OnItemSelectedListener {
-
-        // create array of Strings
-        // and store name of courses
-        String[] courses = { "C", "Data structures",
-                "Interview prep", "Algorithms",
-                "DSA with java", "OS" };
+    public class MainReport extends AppCompatActivity implements
+            AdapterView.OnItemSelectedListener {
+        String[] country = { "A", "B", "C", "D", "E"};
 
         @Override
-        protected void onCreate(Bundle savedInstanceState)
-        {
+        protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            // Take the instance of Spinner and
-            // apply OnItemSelectedListener on it which
-            // tells which item of spinner is clicked
-            Spinner spino = findViewById(R.id.coursesspinner);
+            setContentView(R.layout.report_main);
+            //Getting the instance of Spinner and applying OnItemSelectedListener on it
+            Spinner spin = (Spinner) findViewById(R.id.select_complaint);
             spin.setOnItemSelectedListener(this);
 
-            // Create the instance of ArrayAdapter
-            // having the list of courses
-            ArrayAdapter ad
-                    = new ArrayAdapter(
-                    this,
-                    android.R.layout.simple_spinner_item,
-                    courses);
+            //Creating the ArrayAdapter instance having the country list
+            ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,country);
+            aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            //Setting the ArrayAdapter data on the Spinner
+            spin.setAdapter(aa);
 
-            // set simple layout resource file
-            // for each item of spinner
-            ad.setDropDownViewResource(
-                    android.R.layout
-                            .simple_spinner_dropdown_item);
-
-            // Set the ArrayAdapter (ad) data on the
-            // Spinner which binds data to spinner
-            spino.setAdapter(ad);
         }
-
-        // Performing action when ItemSelected
-        // from spinner, Overriding onItemSelected method
         @Override
-        public void onItemSelected(AdapterView<*> arg0,
-                                   View arg1,
-                                   int position,
-                                   long id)
-        {
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.report_main);
+            //Getting the instance of Spinner and applying OnItemSelectedListener on it
+            Spinner spin = (Spinner) findViewById(R.id.select_department);
+            spin.setOnItemSelectedListener(this);
 
-            // make toastof name of course
-            // which is selected in spinner
-            Toast.makeText(getApplicationContext(),
-                            courses[position],
-                            Toast.LENGTH_LONG)
-                    .show();
+            //Creating the ArrayAdapter instance having the country list
+            ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,country);
+            aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            //Setting the ArrayAdapter data on the Spinner
+            spin.setAdapter(aa);
+
         }
 
+        //Performing action onItemSelected and onNothing selected
         @Override
-        public void onNothingSelected(AdapterView<*> arg0)
-        {
-            // Auto-generated method stub
+        public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+            Toast.makeText(getApplicationContext(),country[position] , Toast.LENGTH_LONG).show();
         }
-    }
+        @Override
+        public void onNothingSelected(AdapterView<?> arg0) {
+            // TODO Auto-generated method stub
+        }
+//    // Main Activity implements Adapter view
+//    public class MainReport
+//            extends AppCompatActivity
+//            implements AdapterView.OnItemSelectedListener {
+//
+//        // create array of Strings
+//        // and store name of courses
+//        String[] courses = { "C", "Data structures",
+//                "Interview prep", "Algorithms",
+//                "DSA with java", "OS" };
+//
+//        @Override
+//        protected void onCreate(Bundle savedInstanceState)
+//        {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.activity_main);
+//
+//            // Take the instance of Spinner and
+//            // apply OnItemSelectedListener on it which
+//            // tells which item of spinner is clicked
+//            Spinner spino = findViewById(R.id.coursesspinner);
+//            spin.setOnItemSelectedListener(this);
+//
+//            // Create the instance of ArrayAdapter
+//            // having the list of courses
+//            ArrayAdapter ad
+//                    = new ArrayAdapter(
+//                    this,
+//                    android.R.layout.simple_spinner_item,
+//                    courses);
+//
+//            // set simple layout resource file
+//            // for each item of spinner
+//            ad.setDropDownViewResource(
+//                    android.R.layout
+//                            .simple_spinner_dropdown_item);
+//
+//            // Set the ArrayAdapter (ad) data on the
+//            // Spinner which binds data to spinner
+//            spino.setAdapter(ad);
+//        }
+//
+//        // Performing action when ItemSelected
+//        // from spinner, Overriding onItemSelected method
+//        @Override
+//        public void onItemSelected(AdapterView<*> arg0,
+//                                   View arg1,
+//                                   int position,
+//                                   long id)
+//        {
+//
+//            // make toastof name of course
+//            // which is selected in spinner
+//            Toast.makeText(getApplicationContext(),
+//                            courses[position],
+//                            Toast.LENGTH_LONG)
+//                    .show();
+//        }
+//
+//        @Override
+//        public void onNothingSelected(AdapterView<*> arg0)
+//        {
+//            // Auto-generated method stub
+//        }
+//    }
 }
