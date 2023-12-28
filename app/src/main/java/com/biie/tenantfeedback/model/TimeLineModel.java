e44e8696249d01da290cb53bbd8fcc168fab82dd
Daffa <daffasyah7890@gmail.com>
2023-12-28 16:53:49 +0700

    trying timeline bg ari

---
+++ app/src/main/java/com/biie/tenantfeedback/model/TimeLineModel.java
@@ -0,0 +1,40 @@
+package com.biie.tenantfeedback.model;
+
+public class TimeLineModel {
+    private String name;
+    private String status;
+    private String description;
+    private String time;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
